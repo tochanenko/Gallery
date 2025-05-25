0afde09b1b8755394c1692b199af204aa2e29e52
Vladyslav Tochanenko <itochanenko@gmail.com>
2025-05-25 18:26:26 +0300

    Show rating for current user

--- gallery-fe/src/pages/Photo.jsx
+++ gallery-fe/src/pages/Photo.jsx
@@ -1,5 +1,5 @@
-import { Suspense, useEffect, useState } from "react";
-import { Await, useRouteLoaderData } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useRouteLoaderData } from "react-router-dom";
 import { API_URL, PHOTO_URL } from "../utils/constants";
 
 import classes from "./Photo.module.scss";
@@ -17,6 +17,24 @@ export default function PhotoPage() {
     return ratings.reduce((acc, curr) => acc + curr.rating, 0) / ratings.length;
   }
 
+  function calculatePercent(ratings, circle) {
+    const rating = calculateRating(ratings);
+    const diff = circle - rating;
+    if (diff <= 0) {
+      return 100;
+    } else if (diff > 1) {
+      return 0;
+    } else {
+      return 100 - diff * 100;
+    }
+  }
+
+  function getRatingForUser(ratings) {
+    const userId = localStorage.getItem("userUUID");
+    const ratingIndex = ratings.findIndex(rating => rating.userId === userId);
+    return ratingIndex !== -1 ? ratings[ratingIndex].rating : 0;
+  }
+
   function formatDate(date) {
     const formattedDate = Date.parse(date);
     return new Intl.DateTimeFormat('en-GB', {
@@ -24,23 +42,6 @@ export default function PhotoPage() {
       month: 'long',
       year: 'numeric',
     }).format(formattedDate);
-  }
-
-  async function handleRating2(newRating) {
-    const userId = localStorage.getItem("userUUID");
-    const response = await fetch(`${API_URL}/rating/${photo.id}`, {
-      method: 'PUT',
-      body: JSON.stringify({ userId, rating: newRating }),
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    });
-
-    if (!response.ok) {
-      throw new Response(JSON.stringify({ message: 'Cound not update rating' }, { status: 500 }));
-    } else {
-      // TODO: Add Optimistic update of ratings array of "photo" object. If the rating with the same userID is in photos.ratings, then replace it with new. If no object with selected userId, put new in photos.ratings
-    }
   }
 
   async function handleRating(newRating) {
@@ -88,23 +89,21 @@ export default function PhotoPage() {
       <div className={classes.photo_info}>
         <h2 className={classes.title}>{photo.title}</h2>
         <div className={classes.details}>
-          <span className={classes.rating}>
-            {[0, 1, 2, 3, 4].map((circle => (
-              <svg
-                key={circle}
-                className={`${calculateRating(photo.ratings) >= circle + 1 ? classes.filled : ''} ${(hoveredIndex !== null) && (hoveredIndex >= circle) ? classes.hovered : ''}`}
-                onMouseEnter={() => setHoveredIndex(circle)}
-                onMouseLeave={() => setHoveredIndex(null)}
-                onClick={() => handleRating(circle + 1, photo.id)}
-                xmlns="http://www.w3.org/2000/svg"
-                version="1.1"
-                viewBox="0 0 24 24"
-              >
-                <path className={classes.st0} d="M12,22.5c-5.79,0-10.5-4.71-10.5-10.5S6.21,1.5,12,1.5s10.5,4.71,10.5,10.5-4.71,10.5-10.5,10.5Z" />
-                <path className={classes.st1} d="M12,3c4.96,0,9,4.04,9,9s-4.04,9-9,9S3,16.96,3,12,7.04,3,12,3M12,0C5.37,0,0,5.37,0,12s5.37,12,12,12,12-5.37,12-12S18.63,0,12,0h0Z" />
-              </svg>
-            )))}
-          </span>
+          {[1, 2, 3, 4, 5].map(circle => (
+            <div
+              key={circle}
+              className={`${classes.rating} ${(hoveredIndex !== null) && (hoveredIndex >= circle) ? classes.hovered : ''}`}
+              onMouseEnter={() => setHoveredIndex(circle)}
+              onMouseLeave={() => setHoveredIndex(null)}
+              onClick={() => handleRating(circle, photo.id)}
+            >
+              <div
+                className={classes.rating_circle}
+                style={{ background: `linear-gradient(to right, ${getRatingForUser(photo.ratings) >= circle ? 'rgba(255, 165, 255, 1)' : 'rgba(0, 0, 0, 1)'} ${calculatePercent(photo.ratings, circle)}%, ${getRatingForUser(photo.ratings) >= circle ? 'rgba(255, 165, 255, 0.5)' : 'rgba(0, 0, 0, 0.5)'} ${calculatePercent(photo.ratings, circle)}%` }}
+              />
+            </div>
+          ))}
+          <span>{calculateRating(photo.ratings) ? calculateRating(photo.ratings) : ''}</span>
           <span className={classes.date}>{formatDate(photo.date)}</span>
         </div>
         <p>{photo.description}</p>
